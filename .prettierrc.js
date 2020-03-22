acc58d5c66f79fdf0b335251d8be780e9a73bf73
Dzianis Davydau <dzianis_davydau@epam.com>
2020-03-22 18:33:07 +0300

    feat: configure format on save

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,9 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'all',
+    bracketSpacing: false,
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    tabWidth: 4,
+    printWidth: 120,
+    useTabs: false,
 };
