18c547ad8563aa782ec95f5c64918a905c61795c
Dzianis Davydau <dzianis_davydau@epam.com>
2020-03-28 10:07:59 +0300

    fix: remove reactotron from code base due to unable to connect virtual device

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow
  */
 
-import React from 'react';
+import React, {Component} from 'react';
 import {SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar} from 'react-native';
 
 import {
@@ -21,55 +21,55 @@ import {Provider} from 'react-redux';
 import Counter from './components/counter/counter.component';
 import {appStore} from './app/store';
 
-if (__DEV__) {
-    import('./ReactotronConfig').then(() => console.log('Reactotron Configured'));
+class App extends Component {
+    render() {
+        return (
+            <Provider store={appStore}>
+                <StatusBar barStyle="dark-content" />
+                <SafeAreaView>
+                    <ScrollView contentInsetAdjustmentBehavior="automatic" style={styles.scrollView}>
+                        <Header />
+                        {global.HermesInternal == null ? null : (
+                            <View style={styles.engine}>
+                                <Text style={styles.footer}>Engine: Hermes</Text>
+                            </View>
+                        )}
+                        <View style={styles.body}>
+                            <Counter />
+                            <View style={styles.sectionContainer}>
+                                <Text style={styles.sectionTitle}>Step One</Text>
+
+                                <Text style={styles.sectionDescription}>
+                                    Edit <Text style={styles.highlight}>App.js</Text> to change this screen and then
+                                    come back to see your edits.
+                                </Text>
+                            </View>
+                            <View style={styles.sectionContainer}>
+                                <Text style={styles.sectionTitle}>See Your Changes</Text>
+                                <Text style={styles.sectionDescription}>
+                                    <ReloadInstructions />
+                                </Text>
+                            </View>
+                            <View style={styles.sectionContainer}>
+                                <Text style={styles.sectionTitle}>Debug</Text>
+                                <Text style={styles.sectionDescription}>
+                                    <DebugInstructions />
+                                </Text>
+                            </View>
+                            <View style={styles.sectionContainer}>
+                                <Text style={styles.sectionTitle}>Learn More</Text>
+                                <Text style={styles.sectionDescription}>
+                                    Read the docs to discover what to do next:
+                                </Text>
+                            </View>
+                            <LearnMoreLinks />
+                        </View>
+                    </ScrollView>
+                </SafeAreaView>
+            </Provider>
+        );
+    }
 }
-
-const App = () => {
-    return (
-        <Provider store={appStore}>
-            <StatusBar barStyle="dark-content" />
-            <SafeAreaView>
-                <ScrollView contentInsetAdjustmentBehavior="automatic" style={styles.scrollView}>
-                    <Header />
-                    {global.HermesInternal == null ? null : (
-                        <View style={styles.engine}>
-                            <Text style={styles.footer}>Engine: Hermes</Text>
-                        </View>
-                    )}
-                    <View style={styles.body}>
-                        <Counter />
-                        <View style={styles.sectionContainer}>
-                            <Text style={styles.sectionTitle}>Step One</Text>
-
-                            <Text style={styles.sectionDescription}>
-                                Edit <Text style={styles.highlight}>App.js</Text> to change this screen and then come
-                                back to see your edits.
-                            </Text>
-                        </View>
-                        <View style={styles.sectionContainer}>
-                            <Text style={styles.sectionTitle}>See Your Changes</Text>
-                            <Text style={styles.sectionDescription}>
-                                <ReloadInstructions />
-                            </Text>
-                        </View>
-                        <View style={styles.sectionContainer}>
-                            <Text style={styles.sectionTitle}>Debug</Text>
-                            <Text style={styles.sectionDescription}>
-                                <DebugInstructions />
-                            </Text>
-                        </View>
-                        <View style={styles.sectionContainer}>
-                            <Text style={styles.sectionTitle}>Learn More</Text>
-                            <Text style={styles.sectionDescription}>Read the docs to discover what to do next:</Text>
-                        </View>
-                        <LearnMoreLinks />
-                    </View>
-                </ScrollView>
-            </SafeAreaView>
-        </Provider>
-    );
-};
 
 const styles = StyleSheet.create({
     scrollView: {
