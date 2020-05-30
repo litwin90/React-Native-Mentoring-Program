47f04b5504cdf32368b3520f71af3706def9cf3e
Dzianis Davydau <dzianis_davydau@epam.com>
2020-05-30 20:55:07 +0300

    feat: integrate crash reporting tool Sentry (HomeTask9)

--- App.js
+++ App.js
@@ -6,6 +6,12 @@ import { Platform, UIManager, YellowBox } from 'react-native';
 import { appStore } from './app/store';
 import AppNavigation from './components/app-navigation/app-navigation';
 
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({
+    dsn: 'https://f6b3859e99a84687abea69eaaf47fc2d@o400464.ingest.sentry.io/5258927',
+});
+
 YellowBox.ignoreWarnings(['Require cycle:']);
 
 if (Platform.OS === 'android' && UIManager.setLayoutAnimationEnabledExperimental) {
